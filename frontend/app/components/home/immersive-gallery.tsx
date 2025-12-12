2142a52efc5e7bd14bad3b84fd95fe97c25a5ed1
SrDefosse <darioarturo.hdz99@gmail.com>
2025-12-12 15:03:51 -0600

    cambios de dimensiones en immersive gallery

--- frontend/app/components/home/immersive-gallery.tsx
+++ frontend/app/components/home/immersive-gallery.tsx
@@ -24,13 +24,13 @@ const DEFAULT_IMAGES: IPicture[] = [
 ];
 
 const IMAGE_STYLES = [
-  "w-[25vw] h-[14.0625vw] md:h-[25vh]",
-  "w-[35vw] h-[16.875vw] md:h-[30vh] -top-[10vh] left-[5vw]",
-  "w-[20vw] h-[30.9375vw] md:h-[55vh] -top-[2vh] -left-[25vw]",
-  "w-[25vw] h-[14.0625vw] md:h-[25vh] -top-[2vh] left-[27.5vw]",
-  "w-[20vw] h-[16.875vw] md:h-[30vh] top-[10vh] left-[5vw]",
-  "w-[30vw] h-[14.0625vw] md:h-[25vh] top-[10vh] -left-[22.5vw]",
-  "w-[20vw] h-[15vw] md:h-[15vh] top-[6vh] left-[28vw]",
+  "w-[25vw] md:w-[25vw] h-[14.0625vw] md:h-[25vh]",
+  "w-[35vw] md:w-[35vw] h-[16.875vw] md:h-[30vh] -top-[10vh] md:-top-[30vh] left-[5vw] md:left-[5vw]",
+  "w-[20vw] md:w-[20vw] h-[30.9375vw] md:h-[55vh] -top-[2vh] md:-top-[15vh] -left-[25vw] md:-left-[25vw]",
+  "w-[25vw] md:w-[25vw] h-[14.0625vw] md:h-[25vh] -top-[2vh] left-[27.5vw] md:left-[27.5vw]",
+  "w-[20vw] md:w-[20vw] h-[16.875vw] md:h-[30vh] top-[10vh] md:top-[30vh] left-[5vw] md:left-[5vw]",
+  "w-[30vw] md:w-[30vw] h-[14.0625vw] md:h-[25vh] top-[10vh] md:top-[27.5vh] -left-[22.5vw] md:-left-[22.5vw]",
+  "w-[20vw] md:w-[15vw] h-[15vw] md:h-[15vh] top-[6vh] md:top-[22.5vh] left-[28vw] md:left-[25vw]",
 ];
 
 const ImmersiveScrollGallery: React.FC<ImmersiveScrollGalleryProps> = ({
